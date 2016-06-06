d8cfcabf35741ae0bd04cc847b6f05badc8557f9
PoeticBliss <meremezeukah@gmail.com>
2016-06-06 10:05:08 -0400

    added getjson method to show result

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ require 'inc/tojson.php';
 
 $json = new tojson('insurance.csv');
 
-print_r(json_encode($json->json));
+print_r($json->json);
 
 //$arr = array_map('str_getcsv', file("insurance.csv"));
 //print_r(json_encode($arr));
