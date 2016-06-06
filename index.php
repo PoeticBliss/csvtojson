0b7f4e664e739e5a823056a7f7167eb5e9c67a33
PoeticBliss <meremezeukah@gmail.com>
2016-06-06 09:59:47 -0400

    created encoding of json result

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+require 'inc/tojson.php';
+
+$json = new tojson('insurance.csv');
+
+print_r(json_encode($json->json));
+
+//$arr = array_map('str_getcsv', file("insurance.csv"));
+//print_r(json_encode($arr));
+
+ ?>
